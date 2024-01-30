decb145045aad017ddecc64571d8563245c11148
Teshome Hailu <teshe2008@hotmail.com>
2024-01-30 13:43:58 +0300

    first commit

---
+++ src/main/java/com/eminent/schoola/SchoolAApplication.java
@@ -0,0 +1,13 @@
+package com.eminent.schoola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eminent/schoola/SchoolAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eminent.schoola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
